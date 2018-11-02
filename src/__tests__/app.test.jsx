79fc8acf9c610e1b9254091d420d5b9ba5bba622
Mystique <helenk530@yahoo.com>
2018-11-01 20:54:17 -0700

    repair test file

--- src/__tests__/app.test.jsx
+++ src/__tests__/app.test.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Routes from '../routes';
+import { Provider } from "react-redux";
+import store from "../stores/Root";
 
 configure({ adapter: new Adapter() });
 
 describe('app', () => {
   it('renders without crashing', () => {
-    mount(<Routes />);
+    mount(
+    <Provider store={store}>
+      <Routes />
+    </Provider>);
   });
 });
