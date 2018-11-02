88f66cd83dfe7c0b52059cd927a71787e6567961
Mystique <helenk530@yahoo.com>
2018-11-01 21:19:35 -0700

    update Launch to a functional component

--- src/components/Launch.jsx
+++ src/components/Launch.jsx
@@ -4,59 +4,30 @@ import PropTypes from 'prop-types';
 const Launch = ({launch, rocket, fetchRocket}) => {
 
   return (
-  
-    <li onClick = { fetchRocket }>
-
+    <li>
       <h2> { launch.mission_name } </h2>
       <div> { launch.rocket.rocket_name } </div>
       <div > { launch.rocket.rocket_id } </div>
+      <button type="submit" onClick={fetchRocket}>Show Rocket Info</button>
       { rocket &&  <div>
         <div> rocket { rocket.cost_per_launch } </div>
         <div>
           { rocket.flickr_images.map((url) => <img height="50px" style={{padding: '20px', border: '2px solid black'}} src={url} />  ) }
         </div>
       </div> }
-
-
     </li>
   );
 }
 
 Launch.propTypes = {
-  launch: PropTypes.object.isRequired,
-  rocket: PropTypes.any.isRequired,
+  launch: PropTypes.shape({
+    mission_name: PropTypes.string,
+    // others
+  }).isRequired,
+  rocket: PropTypes.shape({
+    // stuff
+  }).isRequired,
   fetchRocket: PropTypes.func.isRequired
 };
 
 export default Launch;
-
-// class Launch extends Component {
-//   componentDidMount() {
-//     const { dispatch, launchesCollection } = this.props;
-//     //fetchLaunchesIfNeeded({ dispatch, launchesCollection });
-//   }
-
-//   render() {
-
-//     let launch = this.props.launch;
-//     let fetchRocket = this.props.fetchRocket;
-//     let rocket = this.props.rocket;
-//     return (
-  
-//       <li onClick = { fetchRocket }>
-
-//         <h2> { launch.mission_name } </h2>
-//         <div> { launch.rocket.rocket_name } </div>
-//         <div > { launch.rocket.rocket_id } </div>
-//         { rocket &&  <div>
-//           <div> rocket { rocket.cost_per_launch } </div>
-//           <div>
-//             { rocket.flickr_images.map((url) => <img height="50px" style={{padding: '20px', border: '2px solid black'}} src={url} />  ) }
-//           </div>
-//         </div> }
-
-
-//       </li>
-//     );
-//   }
-// }
